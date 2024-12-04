bbf5607e8d1a7a069dbc3c306016bcdf7b499412
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-04 17:13:37 -0500

    Mapping component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-/**
- * Challenge: create a page that displays your favorite jokes
- * - Create a Joke component in its own file.
- * - Import and render 4-5 <Joke /> components
- * - Each Joke should receive a "setup" prop and a "punchline" prop
- *   and render those however you'd like
- * - Use your favorite 2-part jokes (setup & punchline), or check
- *   jokes.md file for some examples.
- * 
- * EXTRA CREDIT:
- * Some jokes are only a punchline with no setup:
- * 
- * E.g.: "It’s hard to explain puns to kleptomaniacs because 
- * they always take things literally."
- * 
- * If you don't pass in a "question" prop, how might you make it only 
- * show the punchline?
- */
-
 import Joke from "./components/Joke";
+import jokesData from "./jokesData";
+
 
 export default function App() {
+
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />
+  })
+
   return (
     <>
-      <Joke setup={"What lights up a soccer stadium?"} punchline={"A soccer match."} />
-      <Joke setup={"What do sprinters eat before they race?"} punchline={"Nothing. They fast."} />
-      <Joke setup={"Why can't you trust the king of the jungle?"} punchline={"Because he's always lion."} />
-      <Joke setup={"What do sea monsters eat?"} punchline={"Fish and ships."} />
-      <Joke setup={"How did the telephone propose to his girlfriend?"} punchline={"He gave her a ring."} />
-      <Joke punchline={"I was wondering why the frisbee kept getting bigger and bigger, but then it hit me."} />
+      {jokeElements}
 
     </>
   )
 }
+
+{/* <Joke setup={"What lights up a soccer stadium?"} punchline={"A soccer match."} />
+      <Joke setup={"What do sprinters eat before they race?"} punchline={"Nothing. They fast."} />
+      <Joke setup={"Why can't you trust the king of the jungle?"} punchline={"Because he's always lion."} />
+      <Joke setup={"What do sea monsters eat?"} punchline={"Fish and ships."} />
+      <Joke setup={"How did the telephone propose to his girlfriend?"} punchline={"He gave her a ring."} />
+      <Joke punchline={"I was wondering why the frisbee kept getting bigger and bigger, but then it hit me."} /> */}
---
+++ src/jokesData.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        setup: "What lights up a soccer stadium?",
+        punchline: "A soccer match."
+    },
+    {
+        setup: "What do sprinters eat before they race?",
+        punchline: "Nothing. They fast."
+    },
+    {
+        setup: "Why can't you trust the king of the jungle?",
+        punchline: "Because he's always lion."
+    },
+    {
+        setup: "What do sea monsters eat?",
+        punchline: "Fish and ships."
+    },
+    {
+        setup: "How did the telephone propose to his girlfriend?",
+        punchline: "He gave her a ring."
+    },
+    {
+        punchline: "I was wondering why the frisbee kept getting bigger and bigger, but then it hit me."
+    }
+]
