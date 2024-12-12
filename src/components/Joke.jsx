3f17c2600f6d789877b42fc245ca82c190648d9b
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-12 11:32:04 -0500

    Used conditional rendering on puchline

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
 
 export default function Joke(props) {
+
+    const [isShown, setIsShown] = useState(false)
+
+    function toggleShown() {
+        setIsShown((prevState) => {
+            return (
+                !prevState
+            )
+        })
+    }
+
+
+
     return (
         <div>
             {props.setup && <h3>Setup: {props.setup}</h3>}
-            <p>Punchline: {props.punchline}</p>
+            {/* <p>Punchline: {props.punchline}</p> */}
+            {isShown === true ? <p>Punchline: {props.punchline}</p> : ""}
+
+            <button onClick={toggleShown}>Show Punchline</button>
             <hr />
         </div>
     )
--- src/jokesData.js
+++ src/jokesData.js
@@ -1,25 +1,31 @@
 export default [
     {
+        id: 1,
         setup: "What lights up a soccer stadium?",
         punchline: "A soccer match."
     },
     {
+        id: 2,
         setup: "What do sprinters eat before they race?",
         punchline: "Nothing. They fast."
     },
     {
+        id: 3,
         setup: "Why can't you trust the king of the jungle?",
         punchline: "Because he's always lion."
     },
     {
+        id: 4,
         setup: "What do sea monsters eat?",
         punchline: "Fish and ships."
     },
     {
+        id: 5,
         setup: "How did the telephone propose to his girlfriend?",
         punchline: "He gave her a ring."
     },
     {
+        id: 6,
         punchline: "I was wondering why the frisbee kept getting bigger and bigger, but then it hit me."
     }
 ]
